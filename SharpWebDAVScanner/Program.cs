a06251a136d9269f319530de56b2fe5bff49ff70
M0R D4V1D <mordavidwork@gmail.com>
2024-02-19 01:33:27 +0200

    Updated
    Updated

--- SharpWebDAVScanner/Program.cs
+++ SharpWebDAVScanner/Program.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
-namespace SharpWebClientScanner
+namespace SharpWebDAVScanner
 {
     class Program
     {
--- SharpWebDAVScanner/Properties/AssemblyInfo.cs
+++ SharpWebDAVScanner/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpWebClientScanner")]
+[assembly: AssemblyTitle("SharpWebDAVScanner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpWebClientScanner")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyProduct("SharpWebDAVScanner")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
